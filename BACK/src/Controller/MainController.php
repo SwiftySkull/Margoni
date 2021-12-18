15c9088a2c9dc2eec21f61978160b2fcec3204f5
SwiftySkull <aurelien.beneyton@gmail.com>
2021-12-18 01:50:19 +0100

    Edition Form creating

--- BACK/src/Controller/MainController.php
+++ BACK/src/Controller/MainController.php
@@ -3,7 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Painting;
+use App\Form\PaintingType;
+use App\Repository\CategoryRepository;
 use App\Repository\PaintingRepository;
+use App\Repository\TechniqueRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,6 +16,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MainController extends AbstractController
 {
     /**
+     * Home page with all the paintigs, juste for a try
+     * 
      * @Route(
      *      "/",
      *      name="home",
@@ -31,8 +38,10 @@ class MainController extends AbstractController
     }
 
     /**
+     * Description of just one painting
+     * 
      * @Route(
-     *      "/paint/{id<\d+>}",
+     *      "/paint/read/{id<\d+>}",
      *      name="read_paint",
      *      methods={"GET"},
      * )
@@ -43,8 +52,55 @@ class MainController extends AbstractController
             throw $this->createNotFoundException('Oups ! Tableau non trouvé.'); 
         }
 
+        dump($painting);
+
         return $this->render('main/read.html.twig', [
             'painting' => $painting,
+        ]);
+    }
+
+    /**
+     * TODO: Créer la récupération des informations d'une photo TODO:
+     * Form to edit the informations of a painting
+     * 
+     * @Route(
+     *      "/paint/edit/{id<\d+>}",
+     *      name="paint_edit",
+     *      methods={"GET", "POST"},
+     * )
+     */
+    public function edit(Painting $painting = null, $id, Request $request, EntityManagerInterface $em, TechniqueRepository $techniqueRepository, CategoryRepository $categoryRepository)
+    {
+        if (null === $painting) {
+            throw $this->createNotFoundException('Oups ! Tableau non trouvé.'); 
+        }
+
+        $form = $this->createForm(PaintingType::class, $painting);
+
+        $form->handleRequest($request);
+
+        // dd($form->get('techniques')->getData());
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $painting->setPicture($form->get('picture')->getData()->getPathName());
+            for ($i=0; $i < count($form->get('techniques')->getData()); $i++) {
+                $technique = $techniqueRepository->find($form->get('techniques')->getData()[$i]);
+                $painting->addTechniques($technique);
+            }
+
+            for ($i=0; $i < count($form->get('categories')->getData()); $i++) { 
+                $category = $categoryRepository->find($form->get('categories')->getData()[$i]);
+                $painting->addCategories($category);
+            }
+
+            $em->flush();
+
+            return $this->redirectToRoute('read_paint', ['id' => $id]);
+        }
+
+        return $this->render('main/edit.html.twig', [
+            'painting' => $painting,
+            'form' => $form->createView(),
         ]);
     }
 }
